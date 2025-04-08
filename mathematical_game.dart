504de5c074af049bea2b85af17541ef14227b39e
rabab_hamdyy <rababhamdy38@gmail.com>
2025-04-08 06:10:58 +0200

    Levels of the game

--- mathematical_game.dart
+++ mathematical_game.dart
@@ -1,40 +1,58 @@
 import 'dart:io';
 import 'dart:math';
 
-main() {
-  int num1,
-      num2,
-      answer,
-      userAnswer,
-      score = 0,
-      wrongAnswer = 0,
-      totalQuestions = 0;
+void main() {
+  int score = 0;
+  int wrongAnswer = 0;
+  int totalQuestions = 0;
+  int level = 1;
+  double answer;
   String playAgain = "yes";
 
-  print("Welcome to the multiplication game!\n");
-  while (playAgain == "yes") {
-    num1 = Random().nextInt(9) + 1;
-    num2 = Random().nextInt(9) + 1;
-    answer = num1 * num2;
+  print("Welcome to the Math Game!\n");
 
-    print("multiply $num1 * $num2 = ?");
-    userAnswer = int.parse(stdin.readLineSync()!);
-    if (userAnswer == answer) {
-      print("\nCorrect answer!");
-      score += 1;
+  while (playAgain == "yes") {
+    double num1 = Random().nextInt(9 * level) + 1;
+    double num2 = Random().nextInt(9 * level) + 1;
+
+    List<String> operations = ["+", "-", "*", "/"];
+    String symbol = operations[Random().nextInt(operations.length)];
+
+    if (symbol == "/") {
+      num2 = Random().nextInt(9 * level) + 1;
+      num1 = num2 * (Random().nextInt(9 * level) + 1);
+      answer = num1 / num2;
+    } else if (symbol == "+") {
+      answer = num1 + num2;
+    } else if (symbol == "-") {
+      answer = num1 - num2;
     } else {
-      print("Wrong answer! The correct answer is $answer.");
+      answer = num1 * num2;
+    }
+
+    print("Level $level: What is $num1 $symbol $num2 ?");
+    double userAnswer = double.parse(stdin.readLineSync()!);
+
+    if ((userAnswer == answer)) {
+      print("Correct!");
+      score += 1;
+
+      if (score % 3 == 0) {
+        level += 1;
+        print("You've advanced to level $level!");
+      }
+    } else {
+      print("Wrong! The correct answer was $answer.");
       wrongAnswer += 1;
     }
+
     totalQuestions += 1;
 
     print("\nDo you want to play again? (yes/no)");
     playAgain = stdin.readLineSync()!.toLowerCase();
-
   }
 
-  print("Your score: $score out of $totalQuestions");
-  print("Wrong answers: $wrongAnswer");
-  print("Thank you for playing!");
-
+  print("Final Score: $score out of $totalQuestions");
+  print("Wrong Answers: $wrongAnswer");
+  print("Thanks for playing!");
 }
